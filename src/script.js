90b86b447d440a1a47b4d20e9ab47583306750af
Jonathan Cronin <croninjm@gmail.com>
2023-04-09 22:47:54 -0400

    adds rotating tank

--- src/script.js
+++ src/script.js
@@ -1,12 +1,17 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 import './style.css';
 
-/**
- * Base
- */
-// Canvas
+//canvas
 const canvas = document.querySelector('canvas.webgl');
+
+//sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
 //resize
 window.addEventListener('resize', (e)=>{
@@ -23,6 +28,7 @@ window.addEventListener('resize', (e)=>{
     renderer.setPixelRatio(window.devicePixelRatio);
 });
 
+//fullscreen
 window.addEventListener('dblclick', () =>
 {
     const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
@@ -51,51 +57,78 @@ window.addEventListener('dblclick', () =>
     }
 })
 
-// Scene
-const scene = new THREE.Scene()
-
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
+//renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+// Scene
+const scene = new THREE.Scene()
+const environment = new RoomEnvironment();
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+
+
+scene.background = new THREE.Color( 0xbbbbbb );
+scene.environment = pmremGenerator.fromScene( environment ).texture;
+environment.dispose();
+
 /**
- * Animate
+ * Object
  */
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.y = 2;
+// scene.add(mesh)
+
+
+
+//load gltf
+const loader = new GLTFLoader()
+loader.load(
+    'models/m1a1.glb',
+    function (gltf) {
+        // gltf.scene.traverse(function (child) {
+        //     if ((child as THREE.Mesh).isMesh) {
+        //         const m = (child as THREE.Mesh)
+        //         m.receiveShadow = true
+        //         m.castShadow = true
+        //     }
+        //     if (((child as THREE.Light)).isLight) {
+        //         const l = (child as THREE.Light)
+        //         l.castShadow = true
+        //         l.shadow.bias = -.003
+        //         l.shadow.mapSize.width = 2048
+        //         l.shadow.mapSize.height = 2048
+        //     }
+        // })
+        scene.add(gltf.scene)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+
+
+
+//camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 10
+camera.position.y = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+//animate
 const clock = new THREE.Clock()
 
 const tick = () =>
